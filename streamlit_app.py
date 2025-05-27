7fa19f62f05864ce21f5f5dc41d9c4a3484eb9ae
Wulandari465 <desirmonic465@gmail.com>
2025-05-27 11:25:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("monica Desi Retno Wulandari")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "mari belajar ngodingseru"
 )
