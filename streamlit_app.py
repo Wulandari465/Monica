cf11e1136905d201e3cd0b6592346d2119a3dd8a
Wulandari465 <desirmonic465@gmail.com>
2025-05-27 11:43:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title("belajar ngoding bareng monic")
 st.write("mari belajar ngodingseru bersama monic", width=300)
-st.image("IMG-20250526-WA0006.jpg")
+st.image("IMG-20250526-WA0006.jpg", width=300)
